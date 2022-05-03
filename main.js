6f7147dbb1a8652a877a7c1d43c2a58782e3a1ca
StefanoMast <ste.mastrantonio@gmail.com>
2022-05-03 16:26:14 +0200

    add diviso div html

--- main.js
+++ main.js
@@ -47,5 +47,86 @@ const posts = [
 ];
 
 // **Milestone 2** - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+
+//creo un ciclo forEach per ogni elemento che deve essere inserito come div nell'html
+
+//funzione
+
+const container = document.getElementById("container");
+
+posts.forEach(element => {
+    const divCard = divCreator(element.author.image, element.author.name);
+    divCreator(divCard, element.content, element.media)
+    InfoFooterPost(divCard, element.id, element.likes)
+    container.append(divCard)
+});
+
+function divCreator(imgAuthor, nameAuthor) {
+    const divCard = document.createElement("div");
+    divCard.classList.add("post");
+    divCard.innerHTML = 
+//copio tutto il div presente nell'html 
+    `<div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+            </div>
+            <div class="post-meta__data"> <!-- Info profilo -->
+                <div class="post-meta__author">Phil Mangione</div>
+                <div class="post-meta__time">4 mesi fa</div>
+            </div>                    
+        </div>
+    </div>`
+
+    return divCard
+}
+
+//ho diviso il div principale nelle classi di appartenenza, quindi con post_text
+
+function contentDivCard (container, userId, userMedia) {
+    const divScriptContent = document.createElement("div");
+    divScriptContent.classList.add("post_text");
+    divScriptContent.innerHTML = `${userContent}`
+
+//ora assegno la classe post_image
+    const divImage = document.createElement("div");
+    divImage.classList.add("post_image");
+    divImage.innerHTML = `<img src="${userMedia}" alt="${userContent}">`
+
+    //appendo il testo e l'immagine al div
+    container.append(divScriptContent)
+    container.append(divImage)
+
+
+}
+
+//ho diviso il div principale nelle classi di appartenenza, quindi post_footer
+
+function InfoFooterPost (container, userId, userThumbs) {
+    const divInfo = document.createElement("div");
+    divInfo.classList.add("post_footer");
+    divInfo.innerHTML = 
+    `<div class="likes js-likes">
+    <div class= "likes__cta">
+        <a class="like-button js-like-button href="#" data-postid"${userId}>
+            <i class="like-button_icon fas fa-thumbs-uo" aria-hidden="true"><i>
+            <span class="like-button__label">Mi Piace </span>
+        </a>
+    </div>
+    <div class="likes__counter">
+        Piace a <b id="like-counter-1" class="js-likes-counter">${userThumbs}</b> persone
+    </div>
+    </div>`
+    
+    //lo aggiungo al divInfo
+    container.append(divInfo)
+}
+    
+
+
+
+
+
+
 // **Milestone 3** - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
 // Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
